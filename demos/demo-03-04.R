a98872ab0d2d6cfc9478121703cc6c426bb4a6fd
sa-lee <lee.s@wehi.edu.au>
2020-10-15 14:53:25 +1100

    update demos

--- demos/demo-03-04.R
+++ demos/demo-03-04.R
@@ -10,48 +10,41 @@ summary(pcs)$importance[3,1:12]
 
 ## ----pca-xy---------------------------------------------------------
 fake_trees <- dplyr::bind_cols(fake_trees, as.data.frame(pcs$x))
-limn_xy(fake_trees, x = PC1, y = PC2, color = branches)
-
 
 ## ----tsne-xy--------------------------------------------------------
 set.seed(2099)
 tsne <- Rtsne::Rtsne(dplyr::select(fake_trees, dplyr::starts_with("dim")))
 
 tsne_df <- data.frame(tsneX = tsne$Y[,1],
-                      tsneY = tsne$Y[,2],
-                      branches = fake_trees$branches)
-
-limn_xy(tsne_df, x = tsneX, y = tsneY, color = branches)
+                      tsneY = tsne$Y[,2])
 
 # now together
-limn_tour_xylink(
-  dplyr::select(fake_trees, PC1:PC12, branches),
+limn_tour_link(
   tsne_df,
-  x_color = branches,
-  y_color = branches
+  fake_trees,
+  cols = PC1:PC12,
+  color = branches
 )
 
 
-
 ## ----pca-init--------------------------------------------------------
+set.seed(2099)
 tsne_v2 <- Rtsne::Rtsne(
   dplyr::select(fake_trees, dplyr::starts_with("dim")),
   Y_init = clamp_sd(as.matrix(dplyr::select(fake_trees, PC1,PC2)), sd = 1e-4),
   perplexity = nrow(fake_trees) / 100,
-  eta = nrow(fake_trees) / 48,
   max_iter = 3000
 )
 
 tsne_v2_df <- data.frame(
   tsneX = tsne_v2$Y[,1], 
-  tsneY = tsne_v2$Y[,2], 
-  branches = fake_trees$branches
+  tsneY = tsne_v2$Y[,2]
 )
 
-limn_tour_xylink(
-  dplyr::select(fake_trees, PC1:PC12, branches),
+limn_tour_link(
   tsne_v2_df,
-  x_color = branches,
-  y_color = branches
+  fake_trees,
+  cols = PC1:PC12,
+  color = branches
 )
 
